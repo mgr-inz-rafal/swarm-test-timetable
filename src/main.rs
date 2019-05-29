7b868aa47ddf3cfc943789d93f6db769e27f533d
mgr-inz-rafal <rchabowski@gmail.com>
2019-05-29 22:03:24 +0200

    Store textures in hasmap

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-
 extern crate piston_window;
 use piston_window::*;
+use std::collections::HashMap;
+use std::hash::Hash;
 
-fn load_textures(depot: &mut Vec<G2dTexture>, context: &mut G2dTextureContext) {
+const SCREEN_SIZE_NATIVE: [u32; 2] = [1920, 1080];
+
+#[derive(Eq, PartialEq, Hash)]
+enum Textures {
+    Background,
+}
+
+fn load_textures(depot: &mut HashMap<Textures, G2dTexture>, context: &mut G2dTextureContext) {
     let test_image: G2dTexture = Texture::from_path(
         context,
         "images/backgrounds/darlington.jpg",
@@ -11,14 +19,14 @@ fn load_textures(depot: &mut Vec<G2dTexture>, context: &mut G2dTextureContext) {
     )
     .unwrap();
 
-    depot.push(test_image);
+    depot.insert(Textures::Background, test_image);
 }
 
 fn main() {
     let opengl = OpenGL::V3_2;
     let mut window: PistonWindow = WindowSettings::new(
         "Timetable: Demo of the Swarm library by mgr. inż. Rafał",
-        [1920, 1080],
+        SCREEN_SIZE_NATIVE,
     )
     .exit_on_esc(true)
     .fullscreen(true)
@@ -27,13 +35,13 @@ fn main() {
     .unwrap();
 
     let mut ctx = window.create_texture_context();
-    let mut texture_depot = Vec::new();
+    let mut texture_depot = HashMap::new();
     load_textures(&mut texture_depot, &mut ctx);
 
     while let Some(e) = window.next() {
         window.draw_2d(&e, |c, g, _| {
             clear([1.0; 4], g);
-            image(&texture_depot[0], c.transform, g);
+            image(texture_depot.get(&Textures::Background).unwrap(), c.transform, g);
         });
     }
 }
