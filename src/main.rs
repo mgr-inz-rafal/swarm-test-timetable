4fbb27540df596833b4a80deeafca7febda81cb2
mgr-inz-rafal <rchabowski@gmail.com>
2019-06-03 20:07:09 +0200

    Reading demo layout

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
+extern crate swarm;
 extern crate piston_window;
+
 use piston_window::*;
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Read, Result};
 
 const SCREEN_SIZE_NATIVE: [u32; 2] = [1920, 1080];
 const TILE_WIDTH: u32 = 44;
 const TILE_HEIGHT: u32 = 54;
 const TILE_SPACING: u32 = (50 - TILE_WIDTH) / 2;
 const TILES_PER_ROW: u32 = 36;
+const TILES_PER_COLUMN: u32 = 15;
 const BOARD_MARGIN: u32 = (SCREEN_SIZE_NATIVE[0] - (TILE_WIDTH + TILE_SPACING) * TILES_PER_ROW) / 2;
 
 #[derive(Copy, Clone, Eq, PartialEq, Hash)]
@@ -60,7 +65,19 @@ fn load_textures(depot: &mut HashMap<TextureId, G2dTexture>, context: &mut G2dTe
     });
 }
 
-fn main() {
+fn load_layout(id: u32) -> Result<()> {
+    let file = format!("layouts/layout{}.txt", id);
+    println!("Loading layout from '{}'", file);
+    let file = File::open(file)?;
+    let mut buffer = BufReader::new(file);
+    for line in buffer.by_ref().lines() {
+        line.unwrap().chars().enumerate().for_each(|(i, c)| println!("At ({},?) = {}", i, c));
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
     let opengl = OpenGL::V3_2;
     let mut window: PistonWindow = WindowSettings::new(
         "Timetable: Demo of the Swarm library by mgr. inż. Rafał",
@@ -75,6 +92,8 @@ fn main() {
     let mut ctx = window.create_texture_context();
     let mut texture_depot = HashMap::new();
     load_textures(&mut texture_depot, &mut ctx);
+
+    load_layout(1)?;
 
     while let Some(e) = window.next() {
         window.draw_2d(&e, |c, g, _| {
@@ -97,6 +116,7 @@ fn main() {
 
             bert_image.draw(bert_tex, &c.draw_state, c1.transform, g);
         });
-
     }
+
+    Ok(())
 }
